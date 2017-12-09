715ce219cf5d0713196d18578edf5526bd8b62b1
U340709002 <mrtdgny1903@gmail.com>
2017-12-09 19:48:02 +0200

    asdadasd

--- poke/static/js/custom.js
+++ poke/static/js/custom.js
@@ -2,3 +2,16 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
 
+$(document).ready(function() {
+  var imgs = $('.overflow-centered img');//jQuery class selector
+  imgs.each(function(){
+    var img = $(this);
+    var width = img.width(); //jQuery width method
+    var height = img.height(); //jQuery height method
+    if(width < height){
+       img.addClass('portrait');
+    }else{
+       img.addClass('landscape');
+    }
+  })
+});

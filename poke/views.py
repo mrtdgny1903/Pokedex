e538499ad5bbd77c226dc23ebe17baa5a9a3410a
safakoks <safakoksuzer@gmail.com>
2017-12-09 19:11:59 +0300

    Viewler Düzeltildi

--- poke/views.py
+++ poke/views.py
@@ -1,6 +1,6 @@
 from django.views import generic
-from .models import  Pokemon,Trainer
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import  Pokemon
+
 
 class IndexView(generic.ListView):
     template_name = 'poke/index.html'
@@ -12,21 +12,14 @@ class PokemonList(generic.ListView):
     def get_queryset(self):
         return  Pokemon.objects.order_by().all()
 
-class TrainerList(generic.ListView):
-    template_name = 'poke/trainerlist.html'
-    def get_queryset(self):
-        return  Trainer.objects.order_by().all()
-
-class TrainerProfil(generic.DetailView):
-    model = Trainer
-    template_name = 'poke/profil.html'
-
 class PokeDetail(generic.DetailView):
     model = Pokemon
     template_name = 'poke/detail.html'
 
-class StoryView(generic.View):
+
+class StoryView(generic.TemplateView):
     template_name = 'poke/story.html'
+
 
 class GenerationsView(generic.ListView):
     template_name = 'poke/generations.html.html'

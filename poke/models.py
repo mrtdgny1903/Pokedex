07cacee0c3ff5390f66dcdafff0c9f22a4b42021
U340709002 <mrtdgny1903@gmail.com>
2017-12-09 14:51:22 +0200

    asdas

--- poke/models.py
+++ poke/models.py
@@ -2,5 +2,89 @@ from django.db import models
 
 class pokemon(models.Model):
 
-    title = models.CharField(max_length=120)
+    Name = models.CharField(max_length=40)
+    Picture =models.CharField(max_length=120)
+    Height = models.IntegerField()
+    Weight = models.IntegerField()
+    Abilities =models.CharField(max_length=40)
+    Deff = models.IntegerField()
+    Attack = models.IntegerField()
+    Speed = models.IntegerField()
+    Appearence = models.CharField(max_length=120)
+
+    Type = (
+        ('0', 'Fire'),
+        ('1', 'Water'),
+        ('2', 'Poison'),
+        ('3', 'Grass'),
+        ('4', 'Bug'),
+        ('5', 'Electric'),
+        ('6', 'Ice'),
+        ('7', 'Fairy'),
+        ('8', 'Ground'),
+        ('9', 'Dark'),
+        ('10', 'Psychic'),
+        ('11', 'Fighting'),
+        ('12', 'Steel'),
+        ('13', 'Ghost'),
+        ('14', 'Dragon'),
+        ('15', 'Flying'),
+    )
+    skill_type = models.CharField(max_length=1, choices=Type)
+
+
+
+    Gender = (
+        ('0', 'Male'),
+        ('1', 'Female'),
+
+    )
+    gender_type = models.CharField(max_length=1, choices=Gender)
+
+    Generation = (
+        ('0', 'I'),
+        ('1', 'II'),
+        ('2', 'III'),
+        ('3', 'IV'),
+        ('4', 'V'),
+        ('5', 'VI'),
+        ('6', 'VII'),
+
+    )
+    generation_type = models.CharField(max_length=1, choices=Generation)
+
+class trainer(models.Model):
+
+    Name = models.CharField(max_length=40)
+    Picture =models.CharField(max_length=120)
+    Appearance = models.CharField(max_length=120)
+
+    Gender = (
+        ('0', 'Male'),
+        ('1', 'Female'),
+
+    )
+    gender_type = models.CharField(max_length=1, choices=Gender)
+
+    Region = (
+        ('0', 'Kanto'),
+        ('1', 'Johto'),
+        ('2', 'Hoenn'),
+        ('3', 'Sinnoh'),
+        ('4', 'Unova'),
+        ('5', 'Kalos'),
+        ('6', 'Alola'),
+
+    )
+    region_type = models.CharField(max_length=1, choices=Region)
+
+
+
+
+
+
+
+
+
+
 
